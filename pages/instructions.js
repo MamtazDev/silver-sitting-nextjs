f9825a5e79810d9ef91eeb1f9c46eb7188c8785e
mohi14 <mohiuddinkhan1409@gmail.com>
2023-06-12 15:19:48 +0600

    some changes done

--- pages/instructions.js
+++ pages/instructions.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Instructions = () => {
-  return <section>This is instructions</section>;
+  return (
+    <section>
+      <div className="">
+        <h3>How to find a nanny for your child</h3>+
+        <p>
+          "Aging people are like museums: It’s not the facade that counts, but
+          the treasures inside"
+        </p>
+        <h6>Jeanne Moreau</h6>
+      </div>
+    </section>
+  );
 };
 
 export default Instructions;
