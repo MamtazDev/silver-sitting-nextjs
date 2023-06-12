25bde8236261f4888387def89d7f121aa01ad4bd
mohi14 <mohiuddinkhan1409@gmail.com>
2023-06-12 13:10:52 +0600

    layout done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import Main from "@/layouts/Main";
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Main>
+      <Component {...pageProps} />
+    </Main>
+  );
 }
