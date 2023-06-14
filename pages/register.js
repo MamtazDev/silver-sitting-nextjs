cc528c5116f50443ece1a5150c6468399b5af2a1
mohi14 <mohiuddinkhan1409@gmail.com>
2023-06-14 10:10:33 +0600

    register page initiate

--- pages/register.js
+++ pages/register.js
@@ -1,7 +1,37 @@
 import React from "react";
+import styles from "@/styles/Register.module.css";
 
 const Register = () => {
-  return <section>this is register</section>;
+  return (
+    <section className={`container mx-auto ${styles.signInMainContainer}`}>
+      <div className={styles.emptyContainer}></div>
+      <div className={styles.mainRegisterContainer}>
+        <form>
+          <h3>Register</h3>
+          <h6>I am...</h6>
+
+          {/* choose person */}
+          <div>
+            <div>
+              <div>
+                <img src="" alt="" />
+                <p>Grandma</p>
+              </div>
+              <input type="" name="" value="" />
+            </div>
+            <div>
+              <div>
+                <img src="" alt="" />
+                <p>Parents</p>
+              </div>
+              <input type="" name="" value="" />
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className={styles.emptyContainer}></div>
+    </section>
+  );
 };
 
 export default Register;
