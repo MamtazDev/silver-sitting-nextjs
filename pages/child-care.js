c0453b5d0c5106c55fff7e7f319fc5c5f90d77a3
mohi14 <mohiuddinkhan1409@gmail.com>
2023-06-15 15:29:26 +0600

    paggination done

--- pages/child-care.js
+++ pages/child-care.js
@@ -2,109 +2,127 @@ import React, { useState } from "react";
 import styles from "@/styles/ChildCare.module.css";
 import children from "../public/assets/images/child-photo.png";
 import qoute from "../public/assets/images/qoute.png";
+import SearchResult from "@/components/ChildCare/SearchResult";
 
 const ChildCare = () => {
   const [lookfor, setLookfor] = useState("");
+  const [step, setStep] = useState(0);
+
+  const handleSerchFormSubmit = (e) => {
+    e.preventDefault();
+    setStep((prev) => prev + 1);
+  };
+
   return (
     <section className={`container mx-auto ${styles.mainContainer}`}>
       {/* banner */}
       <div className={styles.childCareBanner}>
-        <h3>Find childcare for your child here</h3>
+        {step === 0 && <h3>Find childcare for your child here</h3>}
+        {step === 1 && <h3>Search Result</h3>}
       </div>
 
       {/* chilcare form */}
 
-      <div className={styles.contentContainer}>
-        <div className={styles.emptyContainer}></div>
-        <div className={styles.formContainer}>
-          <form>
-            <h6>I'm looking for</h6>
-            <div className={`d-flex ${styles.grannySelction}`}>
-              <div
-                onClick={() => setLookfor("granny")}
-                className={styles.checkBoxContainer}
-              >
-                <input
-                  id="granny"
-                  type="checkbox"
-                  name="lookFor"
-                  value="gg"
-                  checked={lookfor === "granny"}
-                  className={`me-2 ${styles.colorCheckBox}`}
-                />
-                <label htmlFor="granny">Granny</label>
-              </div>
-              <div
-                onClick={() => setLookfor("grandpa")}
-                className={styles.checkBoxContainer}
-              >
-                <input
-                  id="grandpa"
-                  type="checkbox"
-                  name="lookFor"
-                  value="bb"
-                  checked={lookfor === "grandpa"}
-                  className={`me-2 ${styles.colorCheckBox}`}
-                />
-                <label htmlFor="grandpa">Grandpa</label>
-              </div>
-            </div>
-
-            <div className={styles.inputContainer}>
-              <label>Near</label>
-              <input type="text" className="w-100" />
-
-              <div className={styles.distance}>
-                <p>
-                  Up to max. <input type="text" className="mx-3" /> km distance
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.typesContainer}>
-              <h6 className="text-start">I need the following types of care</h6>
-              <div className={styles.typesCheckboxs}>
-                <div className={styles.box1}>
-                  <div className={styles.checkBoxContainer}>
-                    <input type="checkbox" />
-                    <label>Classic child sitting (child age &gt; 1y.)</label>
-                  </div>
-                  <div className={styles.checkBoxContainer}>
-                    <input type="checkbox" />
-                    <label>Classic child sitting (child age &gt; 4y.)</label>
-                  </div>
-                  <div className={styles.checkBoxContainer}>
-                    <input type="checkbox" />
-                    <label>Homework supervision 1. - 4. class</label>
-                  </div>
-                  <div className={styles.checkBoxContainer}>
-                    <input type="checkbox" />
-                    <label>Homework supervision 5. - 7. class</label>
-                  </div>
+      {step === 0 && (
+        <div className={styles.contentContainer}>
+          <div className={styles.emptyContainer}></div>
+          <div className={styles.formContainer}>
+            <form onSubmit={handleSerchFormSubmit}>
+              <h6>I'm looking for</h6>
+              <div className={`d-flex ${styles.grannySelction}`}>
+                <div
+                  onClick={() => setLookfor("granny")}
+                  className={styles.checkBoxContainer}
+                >
+                  <input
+                    id="granny"
+                    type="checkbox"
+                    name="lookFor"
+                    value="gg"
+                    checked={lookfor === "granny"}
+                    className={`me-2 ${styles.colorCheckBox}`}
+                  />
+                  <label htmlFor="granny">Granny</label>
                 </div>
-                <div className={styles.box1}>
-                  <div className={styles.checkBoxContainer}>
-                    <input type="checkbox" />
-                    <label>Cook and bake</label>
-                  </div>
-                  <div className={styles.checkBoxContainer}>
-                    <input type="checkbox" />
-                    <label>Pick up and delivery services</label>
-                  </div>
+                <div
+                  onClick={() => setLookfor("grandpa")}
+                  className={styles.checkBoxContainer}
+                >
+                  <input
+                    id="grandpa"
+                    type="checkbox"
+                    name="lookFor"
+                    value="bb"
+                    checked={lookfor === "grandpa"}
+                    className={`me-2 ${styles.colorCheckBox}`}
+                  />
+                  <label htmlFor="grandpa">Grandpa</label>
                 </div>
               </div>
-            </div>
-            <div className="text-center">
-              <button className={`btn ${styles.formButton}`} type="submit">
-                Start Search
-              </button>
-            </div>
-          </form>
+
+              <div className={styles.inputContainer}>
+                <label>Near</label>
+                <input type="text" className="w-100" />
+
+                <div className={styles.distance}>
+                  <p>
+                    Up to max. <input type="text" className="mx-3" /> km
+                    distance
+                  </p>
+                </div>
+              </div>
+
+              <div className={styles.typesContainer}>
+                <h6 className="text-start">
+                  I need the following types of care
+                </h6>
+                <div className={styles.typesCheckboxs}>
+                  <div className={styles.box1}>
+                    <div className={styles.checkBoxContainer}>
+                      <input type="checkbox" />
+                      <label>Classic child sitting (child age &gt; 1y.)</label>
+                    </div>
+                    <div className={styles.checkBoxContainer}>
+                      <input type="checkbox" />
+                      <label>Classic child sitting (child age &gt; 4y.)</label>
+                    </div>
+                    <div className={styles.checkBoxContainer}>
+                      <input type="checkbox" />
+                      <label>Homework supervision 1. - 4. class</label>
+                    </div>
+                    <div className={styles.checkBoxContainer}>
+                      <input type="checkbox" />
+                      <label>Homework supervision 5. - 7. class</label>
+                    </div>
+                  </div>
+                  <div className={styles.box1}>
+                    <div className={styles.checkBoxContainer}>
+                      <input type="checkbox" />
+                      <label>Cook and bake</label>
+                    </div>
+                    <div className={styles.checkBoxContainer}>
+                      <input type="checkbox" />
+                      <label>Pick up and delivery services</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="text-center">
+                <button className={`btn ${styles.formButton}`} type="submit">
+                  Start Search
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className={styles.imageContainer}>
+            <img src={children.src} alt="" />
+          </div>
         </div>
-        <div className={styles.imageContainer}>
-          <img src={children.src} alt="" />
-        </div>
-      </div>
+      )}
+
+      {/* search result */}
+
+      {step === 1 && <SearchResult />}
     </section>
   );
 };
