2a6c85314e9331178956285770837d2215ecad48
nipaaa <akternipa941@gmail.com>
2023-06-23 16:19:02 +0600

    search again page add

--- pages/register-success-sms.js
+++ pages/register-success-sms.js
@@ -10,12 +10,12 @@ const SuccessSms = () => {
         <img src={sms.src} alt="" />
         <h6>Nice that you have registered with SilverSitting!</h6>
         <p>
-          To complete your registration, please click on the link in the email
-          we just sent you.
+          To complete your registration, please click on the{" "}
+          <Link href="!#">link</Link> in the <br /> email we just sent you.
         </p>
         <p>
-          <span>For information :</span> a <Link href="!#">link</Link> can be a word with a blue
-          background that can be clicked on.
+          <span>For information :</span> a <Link href="!#">link</Link> can be a
+          word with a blue background that can be clicked on.
         </p>
       </div>
     </div>
