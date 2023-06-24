cb523d1134bb907aaf42e7d04791df9490330742
nipaaa <akternipa941@gmail.com>
2023-06-24 14:50:28 +0600

    update

---
+++ components/TermsConditions/General.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const General = () => {
+  return (
+    <div className="terms">
+      <div className="container">
+        <h4>General</h4>
+        <div>
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default General;
---
+++ pages/terms-and-conditions.js
@@ -0,0 +1,12 @@
+import ListBanner from '@/components/TermsConditions/ListBanner';
+import React from 'react';
+
+const TermsConditions = () => {
+    return (
+        <div>
+            <ListBanner/>
+        </div>
+    );
+};
+
+export default TermsConditions;
