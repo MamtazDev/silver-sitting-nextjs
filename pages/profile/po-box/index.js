b51e78b492facc4d713f95a4dbacecae6ff030e3
mohi14 <mohiuddinkhan1409@gmail.com>
2023-06-24 14:48:55 +0600

    sidebar done

---
+++ pages/profile/po-box/index.js
@@ -0,0 +1,10 @@
+import ProfileLayout from "@/layouts/Profile";
+import React from "react";
+
+const POBOX = () => {
+  return <div>this iis p.o box</div>;
+};
+
+POBOX.PageLayout = ProfileLayout;
+
+export default POBOX;
