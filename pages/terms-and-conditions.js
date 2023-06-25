3052329095325f8ef59375b92a523f62902ef7c7
nipaaa <akternipa941@gmail.com>
2023-06-25 12:01:08 +0600

    add terms and condition

--- pages/terms-and-conditions.js
+++ pages/terms-and-conditions.js
@@ -1,5 +1,9 @@
+import AcceptablePolicy from "@/components/TermsConditions/AcceptablePolicy";
+import ConclusionContract from "@/components/TermsConditions/ConclusionContract";
+import DataProtection from "@/components/TermsConditions/DataProtection";
 import General from "@/components/TermsConditions/General";
 import ListBanner from "@/components/TermsConditions/ListBanner";
+import ServiceDescription from "@/components/TermsConditions/ServiceDescription";
 import React from "react";
 
 const TermsConditions = () => {
@@ -7,6 +11,10 @@ const TermsConditions = () => {
     <div>
       <ListBanner />
       <General />
+      <AcceptablePolicy />
+      <ServiceDescription />
+      <ConclusionContract />
+      <DataProtection/>
     </div>
   );
 };
