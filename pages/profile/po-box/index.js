9b406290ad91238b7756f90247b077d08f7b03c3
nipaaa <akternipa941@gmail.com>
2023-06-25 11:00:55 +0600

    add terms page general section

--- pages/profile/po-box/index.js
+++ pages/profile/po-box/index.js
@@ -6,11 +6,12 @@ import React, { useState } from "react";
 const POBOX = () => {
   const [step, setStep] = useState(2);
 
-  return <div className="w-100">
-    {step === 1 && <PoBox />}
-    {step === 2 && <NoSms />}
-    {step === 1 && <PoBox />}
-  </div>;
+  return (
+    <div className="w-100">
+      {step === 1 && <PoBox />}
+      {step === 2 && <NoSms />}
+    </div>
+  );
 };
 
 POBOX.PageLayout = ProfileLayout;
--- pages/terms-and-conditions.js
+++ pages/terms-and-conditions.js
@@ -1,12 +1,14 @@
-import ListBanner from '@/components/TermsConditions/ListBanner';
-import React from 'react';
+import General from "@/components/TermsConditions/General";
+import ListBanner from "@/components/TermsConditions/ListBanner";
+import React from "react";
 
 const TermsConditions = () => {
-    return (
-        <div>
-            <ListBanner/>
-        </div>
-    );
+  return (
+    <div>
+      <ListBanner />
+      <General />
+    </div>
+  );
 };
 
 export default TermsConditions;
