a0ab71d8fcca4f702baaf453e72c7a2c4f0c7640
mohi14 <mohiuddinkhan1409@gmail.com>
2023-06-25 12:02:05 +0600

    routign done with active route in profile page

--- components/Profile/SideBar.js
+++ components/Profile/SideBar.js
@@ -1,36 +1,67 @@
 import React from "react";
 import styles from "@/styles/Sidebar.module.css";
-import pobox from "../../public/assets/icons/pobox.png";
+import poboxActive from "../../public/assets/icons/pobox.png";
+import pobox from "../../public/assets/icons/poboxNotActive.png";
 import profile from "../../public/assets/icons/profile.png";
+import profileActive from "../../public/assets/icons/profileActive.png";
 import settings from "../../public/assets/icons/settings.png";
+import settingsActive from "../../public/assets/icons/settingsActive.png";
 import password from "../../public/assets/icons/changepassword.png";
+import passwordActive from "../../public/assets/icons/passwordActive.png";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const SideBar = () => {
+  const pathName = usePathname();
+
   return (
     <div className={styles.sidebar}>
       <ul>
-        <li className={styles.active}>
+        <li className={pathName == "/profile/po-box" && styles.active}>
           <Link href="/profile/po-box">
-            <img src={pobox.src} alt="" /> P.O. Box
+            <img
+              src={pathName == "/profile/po-box" ? poboxActive.src : pobox.src}
+              alt=""
+            />{" "}
+            P.O. Box
           </Link>
         </li>
-        <li>
+        <li className={pathName == "/profile" && styles.active}>
           {" "}
           <Link href="/profile">
-            <img src={profile.src} alt="" /> Profile
+            <img
+              src={pathName == "/profile" ? profileActive.src : profile.src}
+              alt=""
+            />{" "}
+            Profile
           </Link>
         </li>
-        <li className={styles.active}>
+        <li className={pathName == "/profile/settings" && styles.active}>
           {" "}
-          <Link href="!#">
-            <img src={settings.src} alt="" /> Settings
+          <Link href="/profile/settings">
+            <img
+              src={
+                pathName == "/profile/settings"
+                  ? settingsActive.src
+                  : settings.src
+              }
+              alt=""
+            />{" "}
+            Settings
           </Link>
         </li>
-        <li>
+        <li className={pathName == "/profile/change-password" && styles.active}>
           {" "}
-          <Link href="!#">
-            <img src={password.src} alt="" /> Change password
+          <Link href="/profile/change-password">
+            <img
+              src={
+                pathName == "/profile/change-password"
+                  ? passwordActive.src
+                  : password.src
+              }
+              alt=""
+            />{" "}
+            Change password
           </Link>
         </li>
       </ul>
