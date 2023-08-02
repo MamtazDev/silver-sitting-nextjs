67d041579bc72b510692fa551fd5dfe69447cff1
nipaaa <akternipa941@gmail.com>
2023-08-02 17:56:20 +0600

    about us section optimized

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -13,111 +13,113 @@ import { useSelector } from "react-redux";
 const Header = () => {
   const { user } = useSelector((state) => state.register);
   return (
-    <Navbar className="header" collapseOnSelect expand="lg">
-      <Container>
-        <Link href="/">
-          <Navbar.Brand>
-            <img src={logo.src} alt="logo" />
-          </Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mx-auto">
-            <div className="parent">
-              <button>Pursue</button>
-              <div className="child">
-                <Link href="/whoWeAre">Who we are</Link>
-                <Link href="/blogs">Blog</Link>
-                <Link href="/feedback">Feedback</Link>
-              </div>
-            </div>
-            <div className="parent">
-              <button>For Nannies</button>
-              <div className="child">
-                <Link href="/guidance"> Guidance child caregiver</Link>
-                <Link href="/ChildrenProviderFaq">FAQ</Link>
-              </div>
-            </div>
-            <div className="parent">
-              <button>For Parents</button>
-              <div className="child">
-                <Link href="/instructions"> Instructions</Link>
-                <Link href="/faq">FAQ</Link>
-                <Link href="/child-care"> Search for child care</Link>
-              </div>
-            </div>
-          </Nav>
-          <Nav className="button-navbar">
-            <Form.Select
-              aria-label="Default select example"
-              className="language-dropdown"
-            >
-              <option value="eng">ENG</option>
-              <option value="1">GER</option>
-            </Form.Select>
-            {user ? (
+    <div className="header">
+      <Navbar collapseOnSelect expand="lg">
+        <Container>
+          <Link href="/">
+            <Navbar.Brand>
+              <img src={logo.src} alt="logo" />
+            </Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
               <div className="parent">
-                <Link
-                  href="/profile"
-                  className="login-btn d-flex justify-content-center align-items-center"
-                  style={{ maxWidth: "56px" }}
-                >
-                  <img src={profile.src} alt="" />
-                </Link>
+                <button>Pursue</button>
                 <div className="child">
-                  <Link
-                    href="/profile/po-box"
-                    style={{
-                      display: "flex",
-                      gap: "18px",
-                      alignItems: "center",
-                    }}
-                  >
-                    <img src={poBox.src} alt="" /> P.O. Box
-                  </Link>
-                  <Link
-                    href="/profile"
-                    style={{
-                      display: "flex",
-                      gap: "18px",
-                      alignItems: "center",
-                    }}
-                  >
-                    {" "}
-                    <img src={profileColor.src} alt="" /> Profile
-                  </Link>
-                  <Link
-                    href="/profile/ideas"
-                    style={{
-                      display: "flex",
-                      gap: "18px",
-                      alignItems: "center",
-                    }}
-                  >
-                    <img src={ideas.src} alt="" /> Ideas
-                  </Link>
-                  <Link
-                    href="/logout"
-                    style={{
-                      display: "flex",
-                      gap: "18px",
-                      alignItems: "center",
-                    }}
-                  >
-                    {" "}
-                    <img src={logOut.src} alt="" /> Logout
-                  </Link>
+                  <Link href="/whoWeAre">Who we are</Link>
+                  <Link href="/blogs">Blog</Link>
+                  <Link href="/feedback">Feedback</Link>
                 </div>
               </div>
-            ) : (
-              <Link className="login-btn" href="/login">
-                Login
-              </Link>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              <div className="parent">
+                <button>For Nannies</button>
+                <div className="child">
+                  <Link href="/guidance"> Guidance child caregiver</Link>
+                  <Link href="/ChildrenProviderFaq">FAQ</Link>
+                </div>
+              </div>
+              <div className="parent">
+                <button>For Parents</button>
+                <div className="child">
+                  <Link href="/instructions"> Instructions</Link>
+                  <Link href="/faq">FAQ</Link>
+                  <Link href="/child-care"> Search for child care</Link>
+                </div>
+              </div>
+            </Nav>
+            <Nav className="button-navbar">
+              <Form.Select
+                aria-label="Default select example"
+                className="language-dropdown"
+              >
+                <option value="eng">ENG</option>
+                <option value="1">GER</option>
+              </Form.Select>
+              {user ? (
+                <div className="parent">
+                  <Link
+                    href="/profile"
+                    className="login-btn d-flex justify-content-center align-items-center"
+                    style={{ maxWidth: "56px" }}
+                  >
+                    <img src={profile.src} alt="" />
+                  </Link>
+                  <div className="child">
+                    <Link
+                      href="/profile/po-box"
+                      style={{
+                        display: "flex",
+                        gap: "18px",
+                        alignItems: "center",
+                      }}
+                    >
+                      <img src={poBox.src} alt="" /> P.O. Box
+                    </Link>
+                    <Link
+                      href="/profile"
+                      style={{
+                        display: "flex",
+                        gap: "18px",
+                        alignItems: "center",
+                      }}
+                    >
+                      {" "}
+                      <img src={profileColor.src} alt="" /> Profile
+                    </Link>
+                    <Link
+                      href="/profile/ideas"
+                      style={{
+                        display: "flex",
+                        gap: "18px",
+                        alignItems: "center",
+                      }}
+                    >
+                      <img src={ideas.src} alt="" /> Ideas
+                    </Link>
+                    <Link
+                      href="/logout"
+                      style={{
+                        display: "flex",
+                        gap: "18px",
+                        alignItems: "center",
+                      }}
+                    >
+                      {" "}
+                      <img src={logOut.src} alt="" /> Logout
+                    </Link>
+                  </div>
+                </div>
+              ) : (
+                <Link className="login-btn" href="/login">
+                  Login
+                </Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
