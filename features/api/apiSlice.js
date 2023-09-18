3c9702b6480209d99b12bc8448e981fed9cde244
mohi14 <mohiuddinkhan1409@gmail.com>
2023-09-18 15:31:38 +0600

    baseurl changed

--- features/api/apiSlice.js
+++ features/api/apiSlice.js
@@ -3,8 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000",
+    baseUrl: "http://16.171.138.157:8000",
   }),
   tagTypes: ["User", "Messages"],
   endpoints: (builder) => ({}),
 });
+  
