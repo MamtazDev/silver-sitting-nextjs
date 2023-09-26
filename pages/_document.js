09945573e1a7768f3f216de3377868153f3c65e2
mohi14 <mohiuddinkhan1409@gmail.com>
2023-09-26 18:06:11 +0600

    google translate added

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,23 @@ export default function Document() {
           http-equiv="Content-Security-Policy"
           content="upgrade-insecure-requests"
         ></meta>
+        <script type="text/javascript">
+          function googleTranslateElementInit(){" "}
+          {
+            new window.google.translate.TranslateElement(
+              {
+                pageLanguage: "en",
+                includedLanguages: "en,fr",
+              },
+              "google_translate_element"
+            )
+          }
+        </script>
+
+        <script
+          type="text/javascript"
+          src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+        ></script>
       </Head>
       <body>
         <Main />
